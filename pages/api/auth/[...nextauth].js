19b20f79d4710235682ac4c4d7ca80b08bc1d59d
rencewang <47406212+rencewang@users.noreply.github.com>
2022-06-11 23:42:41 -0700

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth'
 import SpotifyProvider from 'next-auth/providers/spotify'
 
 export default NextAuth({
-  providers: [
+    providers: [
         SpotifyProvider({
             authorization:
                 'https://accounts.spotify.com/authorize?scope=user-top-read,user-read-recently-played,user-read-currently-playing,playlist-read-private,user-library-read&show_dialog=true',
@@ -10,6 +10,7 @@ export default NextAuth({
             clientSecret: process.env.CLIENT_SECRET,
         }),
     ],
+    secret: process.env.NEXTAUTH_SECRET,
     callbacks: {
         async jwt({token, account}) {
             if (account) {
