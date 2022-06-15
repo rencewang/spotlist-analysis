a4a7c47ec79d6a9476da3834107e25401c6e10b6
rencewang <47406212+rencewang@users.noreply.github.com>
2022-06-15 14:06:25 -0700

    display tracks on click

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
 import { getCookies } from 'cookies-next'
 
-import { getPlaylists } from '../utils/spotify'
+
+import { getPlaylists, getTracks } from '../utils/spotify'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const [list, setList] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [loggedIn, setLoggedIn] = useState(false)
+  const [list, setList] = useState([])
+  const [tracks, setTracks] = useState([])
+
+
+  const fillPlaylists = async () => {
+    const playlists_response = await getPlaylists()
+    setList(playlists_response)
+  }
+
+  const fillTracks = async (url) => {
+    const tracks_response = await getTracks(url)
+    setTracks(tracks_response)
+  }
 
   // fill playlists in once when first load
   useEffect(() => {
-    const fillPlaylists = async () => {
-      const playlists = await getPlaylists()
-      setList(playlists)
-    }
-
     // user is logged in if there is a refresh token stored
-    (async() => {
+    (async () => {
       if (getCookies('token').token) {
         await fillPlaylists()
         setLoggedIn(true)
@@ -38,19 +47,23 @@ export default function Home() {
       {isLoading ? <div>loading...</div> : 
       <>
         <header>
-          {loggedIn ? <div>welcome</div> : <a href="/api/login">Sign in</a>} 
+          {loggedIn ? <div>welcome</div> : <Link href="/api/login">Sign in</Link>} 
         </header>
 
         <section id="playlist-listing">
           {list.map((item) => (
             <div key={item.id}>
-              <h1>{item.name}</h1>
+              <button onClick={() => fillTracks(item.tracks.href)}>{item.name}</button>
             </div>
           ))}
         </section>
 
         <section id="playlist-analysis">
-
+          {tracks.map((item) => (
+            <div key={item.track.id}>
+              <div>{item.track.name}</div>
+            </div>
+          ))}
         </section>
       </>}
     </main>
--- utils/spotify.js
+++ utils/spotify.js
@@ -28,3 +28,32 @@ export const getPlaylists = async () => {
     }
     return playlists
 }
+
+export const getTracks = async (tracks_url) => {
+  let tracks = []
+  try {
+    const token_response = await axios.get('/api/token')
+    const response = await fetch(tracks_url, {
+      headers: {
+        Authorization: `Bearer ${token_response.data}`,
+      }
+    })
+    let { items, next } = await response.json()
+    tracks.push(...items)
+
+    while (next) {
+      const nextPage = await fetch(next, {
+        headers: {
+          Authorization: `Bearer ${token_response.data}`,
+        }
+      })
+      const nextPageData = await nextPage.json()
+      items = nextPageData.items
+      next = nextPageData.next
+      tracks.push(...items)
+    }
+  } catch (error) {
+    console.log(error)
+  }
+  return tracks
+}
