3af032194fe4269527d595d6b89a21f738f71496
rencewang <47406212+rencewang@users.noreply.github.com>
2022-06-17 00:19:00 -0700

    add dropdown select for playlists

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,35 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import { getCookies } from 'cookies-next'
-
+import Select from 'react-select'
 
 import { getPlaylists, getTracks, getGenresFromArtists } from '../utils/spotify'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true)
   const [loggedIn, setLoggedIn] = useState(false)
-  const [list, setList] = useState([])
+  
+  useEffect(() => { 
+    // fill playlists in once when first load
+    (async () => {
+      // user is logged in if there is a refresh token stored
+      if (getCookies('token').token) { 
+        await fillPlaylists()
+        setLoggedIn(true)
+      }
+      setIsLoading(false)
+    })()
+  }, [])
+  
+  const [playlists, setPlaylists] = useState([])
   const [tracks, setTracks] = useState([])
   const [artists, setArtists] = useState([])
   const [artistIds, setArtistIds] = useState([])
 
   const fillPlaylists = async () => {
     const playlists_response = await getPlaylists()
-    setList(playlists_response)
+    setPlaylists(playlists_response)
   }
 
   const fillTracksArtists = async (url) => {
@@ -33,17 +45,13 @@ export default function Home() {
     })
   }
 
-  // fill playlists in once when first load
-  useEffect(() => {
-    // user is logged in if there is a refresh token stored
-    (async () => {
-      if (getCookies('token').token) {
-        await fillPlaylists()
-        setLoggedIn(true)
-      }
-      setIsLoading(false)
-    })()
-  }, [])
+  const [selectedPlaylist, setSelectedPlaylist] = useState(null)
+  const playlistOptions = useMemo(() => 
+    playlists.map(playlist => ({
+      value: playlist.tracks.href,
+      label: playlist.name
+    }))
+  , [playlists]);
 
   return (
     <main>
@@ -56,13 +64,30 @@ export default function Home() {
       {isLoading ? <div>loading...</div> : 
       <>
         <header>
-          {loggedIn ? <div>welcome</div> : <Link href="/api/login">Sign in</Link>} 
+          <div>Spotlist</div>
+          {loggedIn 
+          ? 
+          <Select
+            defaultValue={selectedPlaylist}
+            onChange={setSelectedPlaylist}
+            options={playlistOptions}
+          />
+          : 
+          <button><Link href="/api/login">Sign in</Link></button>
+          }
         </header>
+
+
+        {/* <Select
+            defaultValue={selectedOption}
+            onChange={setSelectedOption}
+            options={options}
+          /> */}
 
         <button onClick={() => getGenresFromArtists(artistIds)}>hi</button>
 
         <section id="playlist-listing">
-          {list.map((item, index) => (
+          {playlists.map((item, index) => (
             <div key={index}>
               <button onClick={() => fillTracksArtists(item.tracks.href)}>{item.name}</button>
             </div>
