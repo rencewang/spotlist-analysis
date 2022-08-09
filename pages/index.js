1dc10751b0c94ba159acee309e2fc804284e6002
rencewang <47406212+rencewang@users.noreply.github.com>
2022-08-09 11:27:38 -0700

    spotify quota extension

--- pages/index.js
+++ pages/index.js
@@ -1,161 +1,200 @@
-import React, { useEffect, useMemo, useState, useRef } from 'react'
-import Head from 'next/head'
-import Link from 'next/link'
-import { getCookies } from 'cookies-next'
-import Select from 'react-select'
+import React, { useEffect, useMemo, useState, useRef } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import { getCookies } from 'cookies-next';
+import Select from 'react-select';
 
-import { getPlaylists, getTracks, getGenresFromArtists } from '../utils/spotify'
-import Tracklist from '../utils/tracklist'
-import Analysis from '../utils/analysis'
-import * as Styled from '../styles/general'
+import {
+  getPlaylists,
+  getTracks,
+  getGenresFromArtists,
+} from '../utils/spotify';
+import Tracklist from '../utils/tracklist';
+import Analysis from '../utils/analysis';
+import * as Styled from '../styles/general';
 
 const Home = () => {
-  const [isPlaylistsLoading, setIsPlaylistsLoading] = useState(false)
-  const [isTracksLoading, setIsTracksLoading] = useState(false)
-  const [loggedIn, setLoggedIn] = useState(null)
-  const [onTracklistPage, setOnTracklistPage] = useState(true)
+  const [isPlaylistsLoading, setIsPlaylistsLoading] = useState(false);
+  const [isTracksLoading, setIsTracksLoading] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(null);
+  const [onTracklistPage, setOnTracklistPage] = useState(true);
 
-  useEffect(() => { 
+  useEffect(() => {
     // remove search query in redirect
     if (window.location.search) {
-      window.location.href = `${window.location.origin}${window.location.pathname}`
+      window.location.href = `${window.location.origin}${window.location.pathname}`;
     }
 
     // fill playlists in once when first load
     (async () => {
       // user is logged in if there is a refresh token stored
-      if (getCookies('token').token) { 
-        setLoggedIn(true)
-        await fillPlaylists()
+      if (getCookies('token').token) {
+        setLoggedIn(true);
+        await fillPlaylists();
       } else {
-        setLoggedIn(false)
+        setLoggedIn(false);
       }
-    })()
-  }, [])
+    })();
+  }, []);
 
-  const [playlists, setPlaylists] = useState([])
-  const [tracks, setTracks] = useState([])
-  const [genres, setGenres] = useState([])
-  const [artists, setArtists] = useState([])
-  
+  const [playlists, setPlaylists] = useState([]);
+  const [tracks, setTracks] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const [artists, setArtists] = useState([]);
+
   const fillPlaylists = async () => {
-    setIsPlaylistsLoading(true)
-    const playlists_response = await getPlaylists()
-    setPlaylists(playlists_response)
-    setIsPlaylistsLoading(false)
-  }
+    setIsPlaylistsLoading(true);
+    const playlists_response = await getPlaylists();
+    setPlaylists(playlists_response);
+    setIsPlaylistsLoading(false);
+  };
 
   // dropdown options for selecting playlist
-  const [selectedPlaylist, setSelectedPlaylist] = useState(null)
-  const playlistOptions = useMemo(() => 
-    playlists.map(playlist => ({
-      value: playlist.tracks.href,
-      label: playlist.name
-    }))
-  , [playlists])
+  const [selectedPlaylist, setSelectedPlaylist] = useState(null);
+  const playlistOptions = useMemo(
+    () =>
+      playlists.map((playlist) => ({
+        value: playlist.tracks.href,
+        label: playlist.name,
+      })),
+    [playlists]
+  );
 
   // fill tracks and artists when new playlist selected
   useEffect(() => {
-    if (selectedPlaylist) fillTracksArtistsGenres(selectedPlaylist.value)
-  }, [selectedPlaylist])
+    if (selectedPlaylist) fillTracksArtistsGenres(selectedPlaylist.value);
+  }, [selectedPlaylist]);
 
   const fillTracksArtistsGenres = async (url) => {
-    let artists = []
-    let artists_ids = []
-    let artists_count = {}
+    let artists = [];
+    let artists_ids = [];
+    let artists_count = {};
 
-    setIsTracksLoading(true)
-    const tracks_response = await getTracks(url)
-    setTracks(tracks_response)
-    setIsTracksLoading(false)
+    setIsTracksLoading(true);
+    const tracks_response = await getTracks(url);
+    setTracks(tracks_response);
+    setIsTracksLoading(false);
 
     // get artists from tracks and genres from artists
-    tracks_response.forEach(track => {
-      track.track.artists.forEach(artist => {
-        artists.push(artist)
-        artists_ids.push(artist.id)
-        artists_count[artist.name] = (artists_count[artist.name] || 0) + 1
-      })
-    })
+    tracks_response.forEach((track) => {
+      track.track.artists.forEach((artist) => {
+        artists.push(artist);
+        artists_ids.push(artist.id);
+        artists_count[artist.name] = (artists_count[artist.name] || 0) + 1;
+      });
+    });
 
     // sort artists and genres by occurrence in playlist
-    let genre_response = await getGenresFromArtists(artists_ids)
-    let artists_sort = []
-    let genres_sort = []
-    Object.keys(artists_count).forEach(artist => {
-      artists_sort.push({name: artist, count: artists_count[artist]})
-    })
-    Object.keys(genre_response).forEach(genre => {
-      genres_sort.push({name: genre, count: genre_response[genre]})
-    })
-    artists_sort.sort((a, b) => {return b.count - a.count})
-    genres_sort.sort((a, b) => {return b.count - a.count})
+    let genre_response = await getGenresFromArtists(artists_ids);
+    let artists_sort = [];
+    let genres_sort = [];
+    Object.keys(artists_count).forEach((artist) => {
+      artists_sort.push({ name: artist, count: artists_count[artist] });
+    });
+    Object.keys(genre_response).forEach((genre) => {
+      genres_sort.push({ name: genre, count: genre_response[genre] });
+    });
+    artists_sort.sort((a, b) => {
+      return b.count - a.count;
+    });
+    genres_sort.sort((a, b) => {
+      return b.count - a.count;
+    });
 
-    setArtists(artists_sort)
-    setGenres(genres_sort)
-  }
+    setArtists(artists_sort);
+    setGenres(genres_sort);
+  };
 
-  const copied = useRef(null)
-  const downloaded = useRef(null)
+  const copied = useRef(null);
+  const downloaded = useRef(null);
 
   return (
     <main>
       <Head>
-        <title>Spotlist</title>
+        <title>See Your Tracklists</title>
         <meta name="description" content="Get Playlist Analysis for Spotify" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-      {loggedIn !== null &&
-      <Styled.Container>
-        <Styled.Alert ref={copied}>Copied!</Styled.Alert>
-        <Styled.Alert ref={downloaded}>Download started!</Styled.Alert>
 
-        <Styled.Header>
-          {loggedIn
-            ? <Select 
-              defaultValue={selectedPlaylist} 
-              onChange={setSelectedPlaylist} 
-              options={playlistOptions} 
-              styles={Styled.SelectOptions}
-              isLoading={isPlaylistsLoading}
-              theme={(theme) => Styled.SelectTheme(theme)}
-            />
-            : <div></div>
-          }
+      {loggedIn !== null && (
+        <Styled.Container>
+          <Styled.Alert ref={copied}>Copied!</Styled.Alert>
+          <Styled.Alert ref={downloaded}>Download started!</Styled.Alert>
 
-          <Styled.Flex>
-            <Styled.Button onClick={() => setOnTracklistPage(true)}>Tracklist</Styled.Button>
-            <Styled.Button onClick={() => setOnTracklistPage(false)}>Analysis</Styled.Button>
-            {loggedIn 
-              ? <Link href="/api/logout"><Styled.Button>Sign out</Styled.Button></Link>
-              : <Link href="/api/login"><Styled.Button>Sign in</Styled.Button></Link>
-            }
-          </Styled.Flex>
+          <Styled.Header>
+            {loggedIn ? (
+              <Select
+                defaultValue={selectedPlaylist}
+                onChange={setSelectedPlaylist}
+                options={playlistOptions}
+                styles={Styled.SelectOptions}
+                isLoading={isPlaylistsLoading}
+                theme={(theme) => Styled.SelectTheme(theme)}
+              />
+            ) : (
+              <div></div>
+            )}
 
-        </Styled.Header>
+            <Styled.Flex>
+              <Styled.Button onClick={() => setOnTracklistPage(true)}>
+                Tracklist
+              </Styled.Button>
+              {/* <Styled.Button onClick={() => setOnTracklistPage(false)}>
+                Analysis
+              </Styled.Button> */}
+              {loggedIn ? (
+                <Link href="/api/logout">
+                  <Styled.Button>Sign out</Styled.Button>
+                </Link>
+              ) : (
+                <Link href="/api/login">
+                  <Styled.Button>Sign in</Styled.Button>
+                </Link>
+              )}
+            </Styled.Flex>
+          </Styled.Header>
 
-        <Styled.Content>
-          {isTracksLoading 
-            ? <Styled.FullPage><div>Loading, please wait...</div></Styled.FullPage> 
-            : (tracks.length
-              ? (onTracklistPage 
-                ? <Tracklist name={selectedPlaylist} tracks={tracks} copied={copied} downloaded={downloaded} />
-                : <Analysis name={selectedPlaylist} artists={artists} genres={genres} downloaded={downloaded} /> 
+          <Styled.Content>
+            {isTracksLoading ? (
+              <Styled.FullPage>
+                <div>Loading, please wait...</div>
+              </Styled.FullPage>
+            ) : tracks.length ? (
+              onTracklistPage ? (
+                <Tracklist
+                  name={selectedPlaylist}
+                  tracks={tracks}
+                  copied={copied}
+                  downloaded={downloaded}
+                />
+              ) : (
+                <Analysis
+                  name={selectedPlaylist}
+                  artists={artists}
+                  genres={genres}
+                  downloaded={downloaded}
+                />
               )
-              : (loggedIn 
-                ? (selectedPlaylist 
-                  ? <Styled.FullPage><div>This playlist seems empty, try a different one...</div></Styled.FullPage>
-                  : <Styled.FullPage><div>Select a playlist</div></Styled.FullPage>) 
-                : <Styled.FullPage><div>Log in with Spotify to see your playlists</div></Styled.FullPage>
+            ) : loggedIn ? (
+              selectedPlaylist ? (
+                <Styled.FullPage>
+                  <div>This playlist seems empty, try a different one...</div>
+                </Styled.FullPage>
+              ) : (
+                <Styled.FullPage>
+                  <div>Select a playlist</div>
+                </Styled.FullPage>
               )
-            )
-          }
-        </Styled.Content>
-      </Styled.Container>
-      }
+            ) : (
+              <Styled.FullPage>
+                <div>Log in with Spotify to see your playlists</div>
+              </Styled.FullPage>
+            )}
+          </Styled.Content>
+        </Styled.Container>
+      )}
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- utils/tracklist.js
+++ utils/tracklist.js
@@ -1,128 +1,209 @@
-import React, { useRef } from 'react'
-import { Icon } from '@iconify/react'
+import React, { useRef } from 'react';
+import { Icon } from '@iconify/react';
 
-import * as Styled from '../styles/pages'
-import * as General from '../styles/general'
+import * as Styled from '../styles/pages';
+import * as General from '../styles/general';
 
 // still needed: loading indicator
 
 const Tracklist = (props) => {
-    const {name, tracks, copied, downloaded} = props
+  const { name, tracks, copied, downloaded } = props;
 
-    const TracksToCSV = (tracks) => {
-        let csvTracks = tracks.map(row => 
-            '"' + row.track.name + 
-            '","' + row.track.artists.map(artist => artist.name).join(', ') + 
-            '","' + row.track.album.name + 
-            '",' + row.added_at
-        ).join('\r\n')
-        return ("track_name, artists, album_name, added_at \r\n" + csvTracks)
-    }
+  const TracksToCSV = (tracks) => {
+    let csvTracks = tracks
+      .map(
+        (row) =>
+          '"' +
+          row.track.name +
+          '","' +
+          row.track.artists.map((artist) => artist.name).join(', ') +
+          '","' +
+          row.track.album.name +
+          '",' +
+          row.added_at
+      )
+      .join('\r\n');
+    return 'track_name, artists, album_name, added_at \r\n' + csvTracks;
+  };
 
-    const DownloadCSV = (event, generate_function, name) => {
-        event.preventDefault()
-        // Create a blob
-        var blob = new Blob([generate_function], { type: 'text/csv;charset=utf-8;' })
-        var url = URL.createObjectURL(blob)
-        // Create a link to download it
-        var pom = document.createElement('a')
-        pom.href = url
-        pom.setAttribute('download', `spotlist-tracks-${name}.csv`)
-        pom.click()
-      }
+  const DownloadCSV = (event, generate_function, name) => {
+    event.preventDefault();
+    // Create a blob
+    var blob = new Blob([generate_function], {
+      type: 'text/csv;charset=utf-8;',
+    });
+    var url = URL.createObjectURL(blob);
+    // Create a link to download it
+    var pom = document.createElement('a');
+    pom.href = url;
+    pom.setAttribute('download', `spotlist-tracks-${name}.csv`);
+    pom.click();
+  };
 
-    // Copy to clipboard action
-    const CopyToClipboard = (element) => {
-        var range = document.createRange()
-        range.selectNode(document.getElementById( element ))
-        window.getSelection().removeAllRanges()
-        window.getSelection().addRange(range)
-        document.execCommand("copy")
-        window.getSelection().removeAllRanges()
-    }
+  // Copy to clipboard action
+  const CopyToClipboard = (element) => {
+    var range = document.createRange();
+    range.selectNode(document.getElementById(element));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+  };
 
-    // Disply copied & downloaded alert
-    const ShowAlert = (ref) => {
-        ref.current.style.opacity = 1
-        ref.current.style.display = "block"
-        setTimeout(() => ref.current.style.opacity = 0, 500)
-        setTimeout(() => ref.current.style.display = "none", 700)
-    }
+  // Disply copied & downloaded alert
+  const ShowAlert = (ref) => {
+    ref.current.style.opacity = 1;
+    ref.current.style.display = 'block';
+    setTimeout(() => (ref.current.style.opacity = 0), 500);
+    setTimeout(() => (ref.current.style.display = 'none'), 700);
+  };
 
-    // Copy button component
-    const CopyButton = ({ element }) => 
-    <General.Button onClick={() => {CopyToClipboard(element); ShowAlert(copied)}}>
-        <Icon icon="bx:copy" width="20px" />
+  // Copy button component
+  const CopyButton = ({ element }) => (
+    <General.Button
+      onClick={() => {
+        CopyToClipboard(element);
+        ShowAlert(copied);
+      }}
+    >
+      <Icon icon="bx:copy" width="20px" />
     </General.Button>
+  );
 
+  return (
+    <Styled.Page>
+      {/* <General.Button
+        className="download"
+        onClick={(e) => {
+          DownloadCSV(e, TracksToCSV(tracks), name.label);
+          ShowAlert(downloaded);
+        }}
+      >
+        Download tracklist as CSV
+      </General.Button> */}
 
-    return (
-        <Styled.Page>
-            <General.Button className="download" onClick={(e) => {DownloadCSV(e, TracksToCSV(tracks), name.label); ShowAlert(downloaded)}}>Download tracklist as CSV</General.Button>
+      <Styled.Table id="tracktable">
+        <Styled.TracklistTableHead id="trackhead">
+          <tr>
+            <th>#</th>
+            <th>Track</th>
+            <th>Artist</th>
+            <th>Album</th>
+            <th>Added on</th>
+            <th>Link</th>
+          </tr>
+        </Styled.TracklistTableHead>
 
-            <Styled.Table id="tracktable">
-                <Styled.TracklistTableHead id='trackhead'>
-                    <tr>
-                        <th>#</th>
-                        <th>Track</th>
-                        <th>Artist</th>
-                        <th>Album</th>
-                        <th>Added on</th>
-                    </tr>
-                </Styled.TracklistTableHead>
-            
-                <Styled.TracklistTableBody>
-                    {tracks.map((track, index) => ( 
-                        <tr key={index}>
-                            <td label="#">{index+1}</td>
+        <Styled.TracklistTableBody>
+          {tracks.map(
+            (track, index) =>
+              (
+                <tr key={index}>
+                  <td label="#">{index + 1}</td>
 
-                            <td label="Track">
-                                <General.Justified>
-                                    <div id={`${index}trackname`}>
-                                        <General.Link href={track.track ? track.track.external_urls.spotify : null} target="_blank" rel="noopener noreferrer"> {track.track ? track.track.name : null}</General.Link>
-                                    </div>
-                                    <CopyButton element={`${index}trackname`} />
-                                </General.Justified>
-                            </td>
+                  <td label="Track">
+                    <General.Justified>
+                      <div id={`${index}trackname`}>
+                        <General.Link
+                          href={
+                            track.track
+                              ? track.track.external_urls.spotify
+                              : null
+                          }
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {' '}
+                          {track.track ? track.track.name : null}
+                        </General.Link>
+                      </div>
+                      <CopyButton element={`${index}trackname`} />
+                    </General.Justified>
+                  </td>
 
-                            <td label="Artist">
-                                <General.Justified>
-                                    <div id={`${index}artistname`}>
-                                        {track.track 
-                                            ? track.track.artists.map((artist, index) => (
-                                                index === 0 
-                                                    ? <span key={index}>
-                                                        <General.Link href={track.track.artists[index].external_urls.spotify || null} target="_blank" rel="noopener noreferrer">{artist.name}</General.Link>
-                                                    </span> 
-                                                    : <span key={index}>,&nbsp;
-                                                        <General.Link href={track.track.artists[index].external_urls.spotify || null} target="_blank" rel="noopener noreferrer">{artist.name}</General.Link>
-                                                    </span>
-                                            )) 
-                                            : null
-                                        }
-                                    </div>
-                                    <CopyButton element={`${index}artistname`} />
-                                </General.Justified>
-                            </td>
+                  <td label="Artist">
+                    <General.Justified>
+                      <div id={`${index}artistname`}>
+                        {track.track
+                          ? track.track.artists.map((artist, index) =>
+                              index === 0 ? (
+                                <span key={index}>
+                                  <General.Link
+                                    href={
+                                      track.track.artists[index].external_urls
+                                        .spotify || null
+                                    }
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                  >
+                                    {artist.name}
+                                  </General.Link>
+                                </span>
+                              ) : (
+                                <span key={index}>
+                                  ,&nbsp;
+                                  <General.Link
+                                    href={
+                                      track.track.artists[index].external_urls
+                                        .spotify || null
+                                    }
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                  >
+                                    {artist.name}
+                                  </General.Link>
+                                </span>
+                              )
+                            )
+                          : null}
+                      </div>
+                      <CopyButton element={`${index}artistname`} />
+                    </General.Justified>
+                  </td>
 
-                            <td label="Album">
-                                <General.Justified>
-                                    <div id={`${index}albumname`}>
-                                        <General.Link href={track.track ? track.track.album.external_urls.spotify : null} target="_blank" rel="noopener noreferrer">{track.track ? track.track.album.name: null}</General.Link>
-                                    </div>
-                                    <CopyButton element={`${index}albumname`} />
-                                </General.Justified>
-                            </td>
+                  <td label="Album">
+                    <General.Justified>
+                      <div id={`${index}albumname`}>
+                        <General.Link
+                          href={
+                            track.track
+                              ? track.track.album.external_urls.spotify
+                              : null
+                          }
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {track.track ? track.track.album.name : null}
+                        </General.Link>
+                      </div>
+                      <CopyButton element={`${index}albumname`} />
+                    </General.Justified>
+                  </td>
 
-                            <td label="Added on">
-                                {track.added_at.substring(5,7)}/{track.added_at.substring(8,10)}/{track.added_at.substring(0,4)}
-                            </td>
-                        </tr> || null
-                    ))}
-                </Styled.TracklistTableBody>
-            </Styled.Table>
-        </Styled.Page>
-    )
-}
+                  <td label="Added on">
+                    {track.added_at.substring(5, 7)}/
+                    {track.added_at.substring(8, 10)}/
+                    {track.added_at.substring(0, 4)}
+                  </td>
 
-export default Tracklist
+                  <td label="Link">
+                    <a
+                      className="spotify-link"
+                      href={
+                        track.track ? track.track.external_urls.spotify : null
+                      }
+                    >
+                      open on&nbsp;
+                      <img src="/spotify.png" alt="album cover" />
+                    </a>
+                  </td>
+                </tr>
+              ) || null
+          )}
+        </Styled.TracklistTableBody>
+      </Styled.Table>
+    </Styled.Page>
+  );
+};
+
+export default Tracklist;
